b4db404267d250e5b0e4dd0952181414145aae93
Trinity Terry <trinityterry@Trinitys-MacBook-Air.local>
2018-01-15 19:20:04 -0600

    1/15/18

--- class-four-lesson.js
+++ class-four-lesson.js
@@ -1,11 +1,11 @@
 var tellMeImCool = function(){
-    console.log(`tellMeImCool Array: Hello, you are totally cool!!`);
+    console.log(`Hello, you are totally cool!!`);
 }
 
 tellMeImCool();
 
 var arrayOfNames = [`Jisie`, `Teresa`, `Matt`, `David`];
-var arrayOfNames2 = [`Larry`, `Curly`, `Moe`]
+var arrayOfNames2 = [`Larry`, `Curly`, `Moe`];
 
 var sayHello = function(taco){
     for(var i = 0; i< taco.length; i++){
@@ -21,3 +21,31 @@ var sayHello = function(taco){
 
 sayHello(arrayOfNames);
 sayHello(arrayOfNames2);
+
+var meatFactory = function(meat){
+    console.log(`meat: ${meat}`);
+    var nugget = meat + ` nugget`;
+    console.log(`nugget: ${nugget}`)
+    return nugget;
+    //ANythin after a return function will not be executed
+}
+
+var chickenNugget = meatFactory(`Chicken`);
+console.log(chickenNugget);
+
+
+
+var validNonMuggles = [`Harry`, `Hermione`, `Ron`, `Jisie`];
+var validJedi = [`Luke`, `Yoda`, `Matt Windu`, `Matt`];
+
+var checkNonValidUsers = function(user, arrayOfValidUsers){
+    if(arrayOfValidUsers.includes(user)){
+        return `Valid`;
+    } else {
+        return `Non-valid!`;
+    }
+}
+
+console.log(checkNonValidUsers(`Bob`, validNonMuggles));
+var  jedi = checkNonValidUsers(`Matt Windu`, validJedi);
+console.log(jedi);
